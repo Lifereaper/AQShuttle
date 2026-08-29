d405007109a4563f40113648e282f051459b0cac
Lifereaper <yalex6677@gmail.com>
2026-08-29 17:28:27 -0600

    Update DatabaseHelper.cs

--- AQShuttle/DatabaseHelper.cs
+++ AQShuttle/DatabaseHelper.cs
@@ -1,15 +1,13 @@
-﻿using System;
+using System;
 using MySql.Data.MySqlClient;
 
 namespace AQShuttle
 {
     public static class DatabaseHelper
     {
-        // This is the master key to your entire system!
-        // We will update this string once your Host Machine is ready.
-        private static string connectionString = "Server=192.168.1.196;Database=AQShuttleDB;Uid=aqadmin;Pwd=aq123";
+        // Added AllowPublicKeyRetrieval=True; to bypass the strict network security block
+        private static string connectionString = "Server=192.168.1.196;Database=AQShuttleDB;Uid=aqadmin;Pwd=AQ2026AQ!;AllowPublicKeyRetrieval=True;";
 
-        // A simple method that all your screens can use to get a fresh, open connection to the database
         public static MySqlConnection GetConnection()
         {
             MySqlConnection conn = new MySqlConnection(connectionString);
