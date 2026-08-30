7f68f323e2b9a36718f44f046e5d083ecffa45e9
Lifereaper <yalex6677@gmail.com>
2026-08-29 18:08:11 -0600

    Update App.xaml.cs

--- AQShuttle/App.xaml.cs
+++ AQShuttle/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
+using System;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using AutoUpdaterDotNET;
 
 namespace AQShuttle
 {
@@ -9,6 +11,12 @@ namespace AQShuttle
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            // Point AutoUpdater directly to your GitHub update.xml file
+            AutoUpdater.Start("https://raw.githubusercontent.com/Lifereaper/AQShuttle/master/update.xml");
+        }
+    }
 }
